70638530fde7c9d41c964b819a23e74508b6b8b5
kills <kills>
2007-06-11 11:55:01 +0000

    update für R3.3

--- redaxo-addons/guestbook/uninstall.inc.php
+++ redaxo-addons/guestbook/uninstall.inc.php
@@ -5,14 +5,10 @@
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  * @package redaxo3
- * @version $Id: uninstall.inc.php,v 1.1 2007/06/11 11:54:26 kills Exp $
+ * @version $Id: uninstall.inc.php,v 1.2 2007/06/11 11:55:01 kills Exp $
  */
 
 $error = '';
-if ($error == '')
-{
-  $error .= rex_uninstallAddon(dirname(__FILE__).'/uninstall.sql');
-}
 
 if ($error != '')
 {
