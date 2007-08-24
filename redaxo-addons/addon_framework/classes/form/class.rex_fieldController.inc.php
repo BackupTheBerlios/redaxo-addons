6607152600af9db4c9d5af99eece7faec1de45f7
kills <kills>
2007-08-24 16:37:52 +0000

    nicht statische methode rex_sql::escape() geändert auf instance call

--- redaxo-addons/addon_framework/classes/form/class.rex_fieldController.inc.php
+++ redaxo-addons/addon_framework/classes/form/class.rex_fieldController.inc.php
@@ -6,7 +6,7 @@
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  * @package redaxo3
- * @version $Id: class.rex_fieldController.inc.php,v 1.2 2007/08/24 10:35:36 kills Exp $
+ * @version $Id: class.rex_fieldController.inc.php,v 1.3 2007/08/24 16:37:52 kills Exp $
  */
 
 define('CONTROLLER_INSERT_MODE', 1);
@@ -79,7 +79,7 @@ class rexFieldController extends rexFieldContainer
     {
       $value = substr($value, 11);
     }
-    return rex_sql::escape($value);
+    return $this->rexform->sql->escape($value);
   }
 
   function _getWhereString()
