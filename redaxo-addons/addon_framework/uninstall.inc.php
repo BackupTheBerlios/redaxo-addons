8070d0d3666b4aaf8d658de770cfcfa55409466c
kills <kills>
2007-09-02 17:38:17 +0000

    css dateien werden auch gelöscht

--- redaxo-addons/addon_framework/uninstall.inc.php
+++ redaxo-addons/addon_framework/uninstall.inc.php
@@ -1,14 +1,15 @@
 <?php
 
 /**
- * Addon Framework Classes 
+ * Addon Framework Classes
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  * @package redaxo3
- * @version $Id: uninstall.inc.php,v 1.2 2007/01/31 20:20:16 kills Exp $
+ * @version $Id: uninstall.inc.php,v 1.3 2007/09/02 17:38:17 kills Exp $
  */
 
 rex_deleteDir('../files/calendar', true);
+rex_deleteDir('../files/addon_framework', true);
 
 $REX['ADDON']['install']['addon_framework'] = 0;
 // ERRMSG IN CASE: $REX['ADDON']['installmsg']['addon_framework'] = "Deinstallation fehlgeschlagen weil...";
