d81bd11576679c1f44034f0da3a8c2cb43a6f353
kills <kills>
2007-09-09 10:22:19 +0000

    readonlyfield übernimmt die post werte nicht mehr

--- redaxo-addons/addon_framework/classes/form/fields/field.readOnlyField.inc.php
+++ redaxo-addons/addon_framework/classes/form/fields/field.readOnlyField.inc.php
@@ -1,18 +1,18 @@
 <?php
 
 /**
- * Addon Framework Classes
+ * Addon Framework Classes 
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  * @package redaxo3
- * @version $Id: field.readOnlyField.inc.php,v 1.3 2007/09/08 10:24:45 kills Exp $
+ * @version $Id: field.readOnlyField.inc.php,v 1.4 2007/09/09 10:22:19 kills Exp $
  */
 
 class readOnlyField extends rexFormField
 {
   var $format;
   var $format_type;
-
+  
   function readOnlyField($name, $label, $attributes = array (), $id = '')
   {
     $this->rexFormField($name, $label, $attributes, $id);
@@ -41,39 +41,27 @@ class readOnlyField extends rexFormField
   {
     return $this->format_type;
   }
-
-  function getValue()
-  {
-    // Hier werden keine POST Values wieder angezeigt!
-
-    // Werte vom User gesetzt?
-    $userValue = $this->getUserValue(null);
-    if($userValue !== null)
-      return $userValue;
-
-    return $this->getDataSetValue();
-  }
-
+  
   function formatValue()
   {
     $value = $this->getValue();
     $format_type = $this->getFormatType();
     if ($format_type != '')
     {
-      $value = rexFormatter :: format($value, $format_type, $this->getFormat());
+      $value = rex_formatter :: format($value, $format_type, $this->getFormat());
     }
     return $value;
   }
-
+  
   function getAttributes()
   {
     $attributes = parent::getAttributes();
-
+    
     // strip required/invalid css-class attribute, da der user die Werte
     // eines read-only fields sowieso nicht beeinflussen kann
     $attributes = str_replace('class="required"', '', $attributes);
     $attributes = str_replace('class="invalid"', '', $attributes);
-
+    
     return $attributes;
   }
 
