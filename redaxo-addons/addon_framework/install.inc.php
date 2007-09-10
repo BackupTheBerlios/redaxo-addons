285a683970f96ad00a55d51e3c0db357a1563a68
kills <kills>
2007-09-10 16:57:39 +0000

    redundanter code entfernt

--- redaxo-addons/addon_framework/install.inc.php
+++ redaxo-addons/addon_framework/install.inc.php
@@ -5,12 +5,12 @@
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  * @package redaxo3
- * @version $Id: install.inc.php,v 1.5 2007/09/10 16:56:04 kills Exp $
+ * @version $Id: install.inc.php,v 1.6 2007/09/10 16:57:39 kills Exp $
  */
 
 require_once $REX['INCLUDE_PATH'] . '/addons/addon_framework/functions/function_pclzip.inc.php';
 
-$mediaFolder = $REX['MEDIAFOLDER'].'/addon_framework');
+$mediaFolder = $REX['MEDIAFOLDER'].'/addon_framework';
 if(!is_dir($mediaFolder))
   mkdir($mediaFolder);
 
@@ -18,10 +18,10 @@ if(!is_dir($mediaFolder))
 rex_a22_extract_archive('include/addons/addon_framework/js/calendar.zip');
 
 
-$cssFile = $REX['MEDIAFOLDER'].'/addon_framework/common.css';
+$cssFile = '/addon_framework/common.css';
 if(!file_exists($cssFile))
-  copy($REX['INCLUDE_PATH'] . '/addons/addon_framework/css/common.css',
-       $cssFile);
+  copy($REX['INCLUDE_PATH'] . '/addons'. $cssFile,
+       $REX['MEDIAFOLDER'] .$cssFile);
 
 $REX['ADDON']['install']['addon_framework'] = 1;
 
