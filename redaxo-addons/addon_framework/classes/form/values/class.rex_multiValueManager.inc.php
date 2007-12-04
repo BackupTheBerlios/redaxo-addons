8ba2b0073b0bb60a858fe56623bb0f38aa089981
kills <kills>
2007-12-04 15:27:59 +0000

    Bugfix: http://forum.redaxo.de/ftopic7565.html

--- redaxo-addons/addon_framework/classes/form/values/class.rex_multiValueManager.inc.php
+++ redaxo-addons/addon_framework/classes/form/values/class.rex_multiValueManager.inc.php
@@ -4,7 +4,7 @@
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  * @package redaxo3
- * @version $Id: class.rex_multiValueManager.inc.php,v 1.1 2007/09/02 14:00:28 kills Exp $
+ * @version $Id: class.rex_multiValueManager.inc.php,v 1.2 2007/12/04 15:27:59 kills Exp $
  */
 
 /*
@@ -21,7 +21,11 @@ class rex_multiValueManager extends rex_simpleValueManager
   function getInsertValue($value)
   {
     $value = parent::getInsertValue($value);
-    return $value[0];
+
+    if(is_array($value))
+      return $value[0];
+
+    return $value;
   }
 
   function getValue($value)
