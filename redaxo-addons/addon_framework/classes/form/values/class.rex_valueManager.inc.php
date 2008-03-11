8e05f3ff1746607f04ca121988272caa70f77137
kills <kills>
2008-03-11 11:25:28 +0000

    docs

--- redaxo-addons/addon_framework/classes/form/values/class.rex_valueManager.inc.php
+++ redaxo-addons/addon_framework/classes/form/values/class.rex_valueManager.inc.php
@@ -5,7 +5,7 @@
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  * @package redaxo3
- * @version $Id: class.rex_valueManager.inc.php,v 1.1 2007/09/02 14:00:28 kills Exp $
+ * @version $Id: class.rex_valueManager.inc.php,v 1.2 2008/03/11 11:25:28 kills Exp $
  */
 
 /*
@@ -25,11 +25,21 @@ class rex_valueManager
     return $this->formField->getSection();
   }
 
+  /*
+   * Prepariert den Wert $value um ihn im Formular anzuzeigen.
+   * Diese Methode wird sowohl beim 1. Anzeigen, als auch beim wiederanzeigen
+   * nach klick auf den Übernehmenbutton durchlaufen.
+   * @access protected
+   */
   function getValue($value)
   {
     trigger_error('You have to override the method rex_valueManager::getValue', E_USER_ERROR);
   }
 
+  /*
+   * Prepariert den Wert $value um ihn in die DB zu speichern
+   * @access protected
+   */
   function getInsertValue($value)
   {
     trigger_error('You have to override the method rex_valueManager::getInsertValue', E_USER_ERROR);
